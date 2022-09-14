0e95d7fd3acb05c6e32fb4916282a822e41e1f4f
Jesus Hermosilla <hermosilladiaz@gmail.com>
2022-09-13 22:50:52 -0500

    display histogram

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 using namespace std;
 
 Mat drawHistogram(Mat img) {
-  int histsize = 2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2;
+  int histsize = 255;
   int histogram[histsize+1];
 
   for(int i=0; i<histsize; i++)
@@ -18,14 +18,15 @@ Mat drawHistogram(Mat img) {
     for(int x=0; x<img.cols; x++)
       histogram[(int)img.at<uchar>(y,x)]++;
 
-  int hist_w = 800, hist_h = 800;  
+  int hist_w = 512;
+  int hist_h = 400;  
   int bin_w = cvRound((double) hist_w/(histsize+1));
   int max = histogram[0];
   
   // Mat histImage(hist_h, hist_w, CV_16UC1, Scalar(0,0,0));
-  Mat histImage(hist_h, hist_w, CV_8UC1);
+  Mat histImage(hist_h, hist_w, CV_8UC1, Scalar(255, 255, 255));
   
-  for(int i=1; i<histsize+1; i++)
+  for(int i=1; i<(histsize+1); i++)
     if(max < histogram[i])
       max = histogram[i];
 
@@ -33,21 +34,22 @@ Mat drawHistogram(Mat img) {
     histogram[i] = ((double)histogram[i]/max)*histImage.rows;
   
   for(int i=0; i<histsize; i++)
-  line(histImage, Point(bin_w*(i), hist_h), Point(bin_w*(i), hist_h - histogram[i]), Scalar(0,0,0), 2, 8, 0);
+  line(histImage, Point(bin_w*(i), hist_h), Point(bin_w*(i), hist_h - histogram[i]), Scalar(0,0,0), 1, 8, 0);
   
   //imshow("Source image", img);
   return histImage;
 }
 
 int main(int argc, char **argv) {
-  Mat img;
+  Mat img, histogram;
   img = imread("/home/jhermosilla/Proyects backup/C-Party/OpenCV/Images/image.jpg", 0);
   if (!img.data) {
     printf("No image data \n");
     return -1;
   }
-  
-  imshow("Output", img);
+
+  histogram = drawHistogram(img);
+  imshow("Output", histogram);
   waitKey(0);  
   return 0;
 }
