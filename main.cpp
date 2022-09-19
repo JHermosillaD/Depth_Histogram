70820862eabf48e43ac2f66ba9c543abcb35640a
Jesus Hermosilla <hermosilladiaz@gmail.com>
2022-09-19 00:10:08 -0500

    add stats plot

--- main.cpp
+++ main.cpp
@@ -6,38 +6,6 @@
 using namespace std;
 using namespace cv;
 
-struct Statistics {
-  vector<int> histogram;
-  
-  double mean() {
-    double sum = 0, mean;
-    for(int k=0; k<65536; k++)
-      sum += (double)histogram[k];
-    mean = sum/65536.0;
-    return mean;
-  }
-
-  double var(double mean) {
-    double var = 0;
-    for(int k=0; k<65536; k++)
-      var += ((double)histogram[k] - mean)*((double)histogram[k] - mean);
-    var = var/(65536.0-1.0);
-    return var;
-  }
-
-  double sd(double var) {
-    double sd = sqrt(var);
-    return sd;
-  }  
-
-  void printStats(double mean, double var, double sd) {
-    cout << "-----------------------------" << endl;
-    cout << "mean:                 " << mean << endl;
-    cout << "variance:             " << var << endl;
-    cout << "standard deviation:   " << sd << endl;
-    cout << "-----------------------------" << endl;
-  }
-};
 
 void infoImage(Mat img) {
   cout << "image width: " << img.cols << '\n'
@@ -89,40 +57,44 @@ Mat equalizedImage(Mat img, vector<int> equalFunct) {
   return img;
 }
 
-Mat drawHistogram(Mat img, vector<int> histogram) {
+Mat drawHistogram(Mat img, vector<int> histogram, double mean=0, double stddev=0) {
   double maxHist = *max_element(histogram.begin(), histogram.end());
   int hist_w = 512; int hist_h = 400;
   double bin_w = (double)hist_w/65537.0;
   Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0,0,0));
 
-  for(int i = 0; i < 65537; i++)
+  for(int i=0; i<65537; i++)
     histogram[i] = ((double)histogram[i]/maxHist)*hist_h;
       
-  for(int i = 0; i < 65537; i++)
+  for(int i=0; i<65537; i++)
     line(histImage, Point(bin_w*(double)i, hist_h), Point(bin_w*(double)i, hist_h-histogram[i]), Scalar(0,0,255), 1, 8, 0);
 
+  line(histImage, Point(bin_w*mean, hist_h*(mean/mean)), Point(bin_w*mean, 0), Scalar(255,0,0), 1, 8, 0);
+  for(int i=0; i<hist_h; i++) 
+    if ( (i & 1) == 0 ) {
+      drawMarker(histImage, Point(bin_w*(mean+stddev), (mean/mean)*i), Scalar(0,255,0), MARKER_SQUARE, 1, 1, 4);
+      drawMarker(histImage, Point(bin_w*(mean-stddev), (mean/mean)*i), Scalar(0,255,0), MARKER_SQUARE, 1, 1, 4);
+      cout << i << endl;
+    }
+  
   return histImage;
 }   
 		  
 int main(int argc, char** argv) {
 
-  Mat img  = imread("/home/jhermosilla/GitHub/Depth_Histogram/img/square.png", IMREAD_ANYDEPTH);
+  Mat img  = imread("/home/jhermosilla/GitHub/Depth_Histogram/img/person.pgm", IMREAD_ANYDEPTH);
   img.convertTo(img, CV_16U);
-  /*  for(int i = 0; i < img.rows; ++i) {
-    ushort* pixel_val = img.ptr<ushort>(i);
-    for (int j = 0; j < img.cols; ++j) 
-      cout << (int)pixel_val[j] << ",";
-  }
-  cout << endl;
-  cout << "----------------------" << endl;*/
-
+  Scalar mean, stddev;
+  meanStdDev(img, mean, stddev, Mat());
   vector<int> Histogram = makeHistogram(img);
-  Mat imgHistogram = drawHistogram(img, Histogram);
+  Mat imgHistogram = drawHistogram(img, Histogram, mean[0], stddev[0]);
 
   vector<int> equalFunction = Equalizedfunction(img, Histogram);  
   Mat img_equal = equalizedImage(img, equalFunction);
+  Scalar mean_equal, stddev_equal;
+  meanStdDev(img_equal, mean_equal, stddev_equal, Mat());
   vector<int> Histogram_equal = makeHistogram(img_equal);
-  Mat imgHistogram_equal = drawHistogram(img_equal, Histogram_equal);
+  Mat imgHistogram_equal = drawHistogram(img_equal, Histogram_equal, mean_equal[0], stddev_equal[0]);
 
   imshow("Source image", img);
   imshow("Depth Histogram", imgHistogram);
